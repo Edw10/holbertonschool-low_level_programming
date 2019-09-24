7b7876a0368549a3a87f5e5a34c6a4072113460b
Edward Osorno <edward-1094@hotmail.com>
2019-09-24 22:19:12 +0000

    Add 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - prints every minute
+ * _putchar write on screen
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int i;
+	int j;
+	int k;
+	int h;
+
+	for (j = 0; j <= 2; j++)
+	{
+		for (i = 0; i <= 4; i++)
+		{
+				for (h = 0; h <= 5; h++)
+				{
+					for (k = 0; k <= 9; k++)
+					{
+
+						_putchar(j);
+						_putchar(i);
+						_putchar(':');
+						_putchar(h);
+						_putchar(k)
+					}
+				}
+		}
+	}
+	return;
+
+}
