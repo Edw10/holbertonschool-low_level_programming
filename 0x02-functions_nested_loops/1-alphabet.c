296ea1d3c753ab2d35f011678969aaec26d10acb
Edward Osorno <edward-1094@hotmail.com>
2019-09-24 19:35:45 +0000

    Add 1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - prints alphabet on screen
+ * _putchar write on screen
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+		_putchar(i);
+	_putchar('\n');
+	return ();
+
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,2 +1,3 @@
 /* _putchar function write in the screen */
 int _putchar(char c);
+void print_alphabet(void);
