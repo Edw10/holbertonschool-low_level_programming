bdfb8eec6f8233a93cbe485b0eeb0913cf369af6
Edward Osorno <edward-1094@hotmail.com>
2019-09-24 22:44:39 +0000

    Add 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,10 +12,14 @@ void jack_bauer(void)
 	int j = 0;
 	int k = 0;
 	int h = 0;
+	int f = 9;
 
 	for (j = 0; j <= 2; j++)
 	{
-		while (i <= 9 && j <= 2)
+		if (j == 2)
+			f = 3;
+		i = 0;
+		while (i <= f)
 		{
 			for (h = 0; h <= 5; h++)
 			{
