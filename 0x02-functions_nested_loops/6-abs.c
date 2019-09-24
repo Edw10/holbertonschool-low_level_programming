a060aed2f0097e88359db41b66e6f48641de877a
Edward Osorno <edward-1094@hotmail.com>
2019-09-24 20:34:05 +0000

    Add 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,7 +2,7 @@
 
 /**
  * _abs - print the sign of a number
- * @: the number to test
+ * @n: the number to test
  *_putchar write on screen
  * Return: the absolute valor of a number
  */
@@ -11,7 +11,7 @@ int _abs(int n)
 {
 	if (n < 0)
 	{
-		return (n*-1);
+		return (n * -1);
 	}
 	else if (n >=  0)
 	{
