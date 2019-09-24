90dedf815f50bb09fc292b7d117b97b3b16a99ac
Edward Osorno <edward-1094@hotmail.com>
2019-09-24 21:09:15 +0000

    Add 7-print-last-digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - print the las digit of a number
+ * @n: the number to test
+ * _putchar write on screen
+ * Return: the absolute valor of a number
+ */
+
+int print_last_digit(int n)
+{
+	if (n < 0)
+	{
+		_putchar(((n * -1) % 10) + 48);
+		return (((n * -1) % 10));
+	}
+	_putchar((n % 10) + 48);
+	return (n % 10);
+
+}
