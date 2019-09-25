a3027d11403da3f3c89ed2a80ee7e1301e14084d
Edward Osorno <edward-1094@hotmail.com>
2019-09-25 22:47:35 +0000

    Add 100-times

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+
+/**
+ * print_times_table - prints every minute
+ *@n: number of the table
+ * _putchar write on screen
+ * Return: void
+ */
+
+void print_times_table(int n)
+{
+	int j;
+	int h;
+
+	for (j = 0; j <= n; j++)
+	{
+		for (h = 0; h <= n; h++)
+		{
+			if (h == 0)
+			{
+				_putchar('0' + (j * h));
+				_putchar(',');
+			}
+			if ((h * j) >= 100)
+			{
+				_putchar((h * j) / 100 + '0');
+				_putchar((h * j) / 10 + '0');
+				_putchar((h * j) % 10 + '0');
+			}
+			else if ((h * j) >= 10)
+			{
+				_putchar(' ');
+				_putchar((h * j) / 10 + '0');
+				_putchar((h * j) % 10 + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((h * j) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+	return;
+
+}
