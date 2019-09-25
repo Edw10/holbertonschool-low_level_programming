0ad4188c1db7af57491ead0fe11dbc53ac8c991f
Edward Osorno <edward-1094@hotmail.com>
2019-09-25 22:11:21 +0000

    add 3-print

--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -1,4 +1,4 @@
-include <stdio.h>
+#include <stdio.h>
 #include "holberton.h"
 
 /**
@@ -13,9 +13,9 @@ int main(void)
 	int day;
 	int year;
 
-	month = 4;
-	day = 01;
-	year = 1997;
+	month = 2;
+	day = 29;
+	year = 2000;
 
 	printf("Date: %02d/%02d/%04d\n", month, day, year);
 
