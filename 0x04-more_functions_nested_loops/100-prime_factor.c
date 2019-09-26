271d5e6f59577fbcc5e404b7e8eaedfb0a35db84
Edward Osorno <edward-1094@hotmail.com>
2019-09-26 19:31:35 +0000

    add 100-prime

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -9,21 +9,29 @@
 int main(void)
 {
 	long i;
-	long j;
+	long j = 2;
 
-	for (i = 612852475142; i >= 2; i--)
+	for (i = 1231951; i >= 2; i--)
 	{
-		for (j = 2 ; j < i; j++)
+		if (1231952 % i == 0)
 		{
-			if (i % j == 0)
+			/* printf("%ld \n", i);*/
+			for (j = 2 ; j < i; j++)
 			{
+				/* printf("%ld %ld \n", j, i); */
+
+				if (i % j == 0)
+				{
+					/* printf("%ld %ld \n", j, i); */
+					break;
+				}
+			}
+
+			if (j >= i)
+			{
+				printf("%ld\n", i);
 				break;
 			}
-		}
-		if (j >= i)
-		{
-			printf("%ld\n", i);
-			break;
 		}
 	}
 	return (0);
