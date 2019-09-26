48a40be38cb50c12f895e0348d8dfb86179a495e
Edward Osorno <edward-1094@hotmail.com>
2019-09-26 18:33:31 +0000

    add 100-prime

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - find the largest prime number
+ *
+ * Return: always 0;
+ */
+
+int main(void)
+{
+	long i;
+	long j;
+
+	for (i = 612852475142; i >= 2; i--)
+	{
+		for (j = i - 1; j >= 2; j--)
+		{
+			if (i % j == 0)
+			{
+				break;
+			}
+		}
+		if (j == 1)
+		{
+			printf("%ld\n", i);
+			break;
+		}
+	}
+	return (0);
+}
