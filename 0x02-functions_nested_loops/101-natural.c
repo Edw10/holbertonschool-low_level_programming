29d02cfbb630130dd71350c7c211a287bdd7cd59
Edward Osorno <edward-1094@hotmail.com>
2019-09-26 03:56:52 +0000

    Add 101-natu

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,7 +11,7 @@ void main(void)
 	int n = 1;
 	int sum;
 
-	while ( (3 * n) < 1024 || (5 * n) < 1024)
+	while ((3 * n) < 1024 || (5 * n) < 1024)
 	{
 		if ((n * 5) % 3 == 0)
 			sum = sum + (3 * n);
