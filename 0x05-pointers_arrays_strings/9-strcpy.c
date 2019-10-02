70f0104c9ddf06f5266c3d55fdc12cff097669bf
Edward Osorno <edward-1094@hotmail.com>
2019-10-02 04:49:18 +0000

    add 9-strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _strcpy - copy a string
+ * @dest: copy of the string
+ * @src: string to copy
+ * Return: the adress of the pointer dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int c = 0;
+
+	while(*(src + c) != '\0')
+	{
+		*(dest + c) = *(src + c);
+		c++;
+	}
+	*(dest + c) = *(src + c);
+	return (dest);
+}
