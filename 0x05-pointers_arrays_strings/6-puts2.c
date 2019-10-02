d5d2db7a352348d387d7f1073718fa251adb347f
Edward Osorno <edward-1094@hotmail.com>
2019-10-02 03:54:48 +0000

    6-puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * puts2 - print a string in screen
+ * @str: string to print
+ *
+ */
+
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str = str + 2;
+	}
+	_putchar('\n');
+}
