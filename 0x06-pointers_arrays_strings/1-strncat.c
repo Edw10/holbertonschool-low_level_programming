1638e14e5cc16d5f4f582908b67a5ce6cac7ec7a
Edward Osorno <edward-1094@hotmail.com>
2019-10-03 18:25:45 +0000

    add 1-strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 
 /**
- * _strcat - concatenate two strings
+ * _strncat - concatenate two strings
  * @dest: string concatenated
  * @src: string to concatenate
- *
+ * @n: number of bytes
  * Return: the concatenated string
  */
 
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 	int c[2];
 
@@ -20,13 +20,13 @@ char *_strcat(char *dest, char *src)
 		c[0] = c[0] + 1;
 	}
 
-	while (*(src + c[1]) != '\0')
+	while (*(src + c[1]) != '\0' && c[1] < n)
 	{
 		*(dest + c[0]) = *(src + c[1]);
 		c[0] = c[0] + 1;
 		c[1] = c[1] + 1;
 
 	}
-	*(dest + c[0]) = *(src + c[1]);
+	*(dest + c[0]) = '\0';
 	return (dest);
 }
