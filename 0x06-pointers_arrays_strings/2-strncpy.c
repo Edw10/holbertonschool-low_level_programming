5e2b544a15dec364d3b4d5ad4f8230453d60090f
Edward Osorno <edward-1094@hotmail.com>
2019-10-03 20:01:53 +0000

    add 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,6 +5,7 @@
  * @dest: string of destiny
  * @src: string to copy
  * @n: number of bytes
+ * Return: the addres of the string copied
  */
 
 char *_strncpy(char *dest, char *src, int n)
@@ -15,7 +16,7 @@ char *_strncpy(char *dest, char *src, int n)
 	while (*(src + c) != '\0' && c < n)
 	{
 		*(dest + c) = *(src + c);
-		if (*(dest + c) == '\n' && *(src + c + 1) == '\0')
+		if (*(dest + c) == '\n')
 			for (i = c + 1; i < n; i++)
 				*(dest + i) = '\0';
 		c++;
