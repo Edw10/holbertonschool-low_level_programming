bebe541a887fb4ad4d0c691350c5b171c3409353
Edward Osorno <edward-1094@hotmail.com>
2019-10-03 21:17:00 +0000

    add 4-rev_array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - reverse the content of an array
+ * @a: array to reverse
+ * @n: length of the array
+ *
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, aux;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		aux = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = aux;
+	}
+}
