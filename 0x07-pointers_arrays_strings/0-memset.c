f0183a01d354032a729aa515c40eb82bf47af529
Edward Osorno <edward-1094@hotmail.com>
2019-10-07 22:19:07 +0000

    add 0x07

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,11 +5,13 @@
  * @s: memory area to fill
  * @b: byte to put
  * @n: number of spaces to fill
+ * Return: the adders of the first character
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
+	unsigned int i;
+
 	for (i = 0; i <= n; i++)
 		*(s + i) = b;
 	return (s);
