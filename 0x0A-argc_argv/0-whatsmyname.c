f835fa877576e5328bbbd6ddf11dae2a008bb1b7
Edward Osorno <edward-1094@hotmail.com>
2019-10-15 04:55:44 +0000

    argc argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,9 +2,9 @@
 
 /**
  * main - print the name of the program
- * @argc - the argumments count
- * @argv - the arguments array
- * Return - always 0
+ * @argc: the argumments count
+ * @argv: the arguments array
+ * Return: always 0
  */
 
 int main(int argc, char *argv[])
