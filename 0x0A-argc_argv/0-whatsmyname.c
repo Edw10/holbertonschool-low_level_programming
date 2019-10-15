e95f534972e7a90e1d331e656d496aff97ac0d43
Edward Osorno <edward-1094@hotmail.com>
2019-10-15 04:54:07 +0000

    argc argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print the name of the program
+ * @argc - the argumments count
+ * @argv - the arguments array
+ * Return - always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
