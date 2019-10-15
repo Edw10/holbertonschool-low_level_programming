cfadb88e59c2c9cc13e5ea920356f62893ef472b
Edward Osorno <edward-1094@hotmail.com>
2019-10-15 04:57:36 +0000

    1-args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print the name of the program
+ * @argc: the argumments count
+ * @argv: the arguments array
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	(void)argv;
+	return (0);
+}
