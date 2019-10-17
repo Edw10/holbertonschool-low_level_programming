19c828c0bcafb018ba46828547bdcfe6a28791ab
Edward Osorno <edward-1094@hotmail.com>
2019-10-17 20:00:19 +0000

    add

---
+++ 0x0C-more_malloc_free/4-realloc.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p;
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (!ptr)
+		return (malloc(new_size));
+
+	if (old_size == new_size)
+		return (ptr);
+	p = malloc(new_size);
+	if (p)
+	{
+		unsigned int i = 0
+
+		while (p[i] && i < new_size)
+		{
+			p[i] = ptr[i];
+			i++;
+		}
+		free(ptr);
+	}
+	else
+	{
+		return (NULL);
+	}
+	return (p);
+}
