d3182746f95bf735a46f0e5161b4dd115ecdff35
Edward Osorno <edward-1094@hotmail.com>
2019-10-17 20:20:43 +0000

    com

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,7 +12,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		if (new_size == 0)
 		{
-			free(ptr);
 			return (NULL);
 		}
 
@@ -34,7 +33,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		unsigned int i = 0;
 
-		while (p[i] && i < new_size)
+		while (pt[i] && i < new_size)
 		{
 			p[i] = pt[i];
 			i++;
