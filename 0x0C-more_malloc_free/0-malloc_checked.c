9ee29738a725aba391cbd1426b40dd885e090b83
Edward Osorno <edward-1094@hotmail.com>
2019-10-17 15:13:04 +0000

    0-malloc_checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - validate the reserved memory
+ * @b: the size of the memory to reserve
+ * Return: return the pointer to the memory or 98 if fails
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+	p = malloc(b);
+
+	if (p)
+		return (p);
+	else
+		exit (98);
+
+}
