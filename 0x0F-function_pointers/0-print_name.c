2b64fde09ec0df6b797c075faecd93c935dc063b
Edward Osorno <edward-1094@hotmail.com>
2019-10-24 00:24:13 +0000

    add

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 
 /**
  * print_name - calls the correspond function
@@ -8,6 +9,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name)
+	if (name != NULL)
 		f(name);
 }
