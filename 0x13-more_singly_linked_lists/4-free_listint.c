7b34e05a0815f95c25c986851c35ae71214b4f26
Edward Osorno <edward-1094@hotmail.com>
2019-11-06 00:20:21 +0000

    add 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * free_listint - free a linked list
+ * @head: pointer to the list
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *h;
+
+	if (!head)
+		return;
+
+	h = head;
+
+	while (h)
+	{
+		h = h->next;
+		free(head);
+		head = h;
+ 	}
+
+	return;
+}
