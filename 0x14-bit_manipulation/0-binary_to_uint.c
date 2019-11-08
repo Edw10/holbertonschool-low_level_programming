d52470827e8a9fd08453eae0f7953bc9f93d4a37
Edward Osorno <edward-1094@hotmail.com>
2019-11-08 05:34:32 +0000

    add 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <string.h>
 
+int _len(const char *b);
+
 /**
  * binary_to_uint - binary to dec
  * @b: pointer to a string
@@ -14,13 +16,34 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int n = 0;
 	int i;
 
-	for (i = strlen(b) - 1; i >= 0; i--)
+
+	for (i = _len(b); i >= 0; i--)
 	{
+
 		if (*b != '0' && *b != '1')
 			return (0);
+
+
 		if (*b == '1')
 			n = n + (1 << i);
 		b++;
 	}
 	return (n);
+}
+
+/**
+ * _len - length of a string
+ * @b: pointer to string
+ * Return: the length
+ */
+
+
+int _len(const char *b)
+{
+	unsigned int i = 0;
+
+	while (*(b + i))
+		i++;
+
+	return (i - 1);
 }
