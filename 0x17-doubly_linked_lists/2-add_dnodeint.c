50a3f3aca7800cd5526168a5fb2bd837aa907327
Edward Osorno <edward-1094@hotmail.com>
2020-01-29 20:27:13 +0000

    add

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -21,9 +21,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (*head)
 	{
 		(*head)->prev = new;
+		new->next = (*head);
 	}
-	new->next = (*head);
 	new->prev = NULL;
-	head = &new;
+	*head = new;
 	return (new);
 }
