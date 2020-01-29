41d634acef1b9247327bab60f3e590eb51356ed0
Edward Osorno <edward-1094@hotmail.com>
2020-01-29 20:53:31 +0000

    add

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_dlistint - free a list
+ * @head: head of the list
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	while (head)
+	{
+		head = head->next;
+		free(head->prev);
+	}
+}
