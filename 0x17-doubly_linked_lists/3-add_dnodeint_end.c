8eef5f04ed78c3197f33c1f8b437634a6e88539a
Edward Osorno <edward-1094@hotmail.com>
2020-01-29 20:44:38 +0000

    add

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,36 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - return the lenght of a double linked list
+ * @head: head of the double list to print
+ * @n: number
+ * Return: the length of nodes
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new, *aux;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	if (*head)
+	{
+		aux = *head;
+		while (aux->next)
+		{
+			aux = aux->next;
+		}
+		aux->next = new;
+		new->prev = aux;
+	}
+	else
+	{
+		*head = new;
+	}
+	return (new);
+}
